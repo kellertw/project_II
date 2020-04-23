fd4e24d5845541db378bef4317a8d668ac05b8a9
Ben Cliff <bocliffwm@gmail.com>
2020-04-22 20:05:53 -0400

    Adding marker to the map

--- js/logic.js
+++ js/logic.js
@@ -10,3 +10,13 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: "mapbox.streets-basic",
   accessToken: API_KEY
 }).addTo(myMap);
+
+// Create a new marker
+// Pass in some initial options, and then add it to the map using the addTo method
+var marker = L.marker([33.25, -114.67], {
+  draggable: true,
+  title: "My First Marker"
+}).addTo(myMap);
+
+// Binding a pop-up to our marker
+marker.bindPopup("Hello There!");
