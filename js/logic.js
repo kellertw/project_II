8755247cb1bcd3f60bc49b3affa63cc24ccc9e77
kehochman <58151660+kehochman@users.noreply.github.com>
2020-04-24 12:30:05 -0400

    Resized elements in HTML file

--- js/logic.js
+++ js/logic.js
@@ -10,3 +10,33 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: "mapbox.streets-basic",
   accessToken: API_KEY
 }).addTo(myMap);
+
+var csvData;
+
+console.log('Hi!');
+
+
+d3.csv('./data/SARStats_6.csv', data => {
+  csvData = data.splice(0,6);
+
+  var row = d3.select('tbody').append('tr')
+
+  csvData.forEach(obj => {
+    var cell = row.append('td');
+
+    console.log(obj);
+    
+
+    Object.values(obj).forEach(val => {
+      cell.text(val);
+    })
+  });
+
+})
+
+
+// Count: "17"
+// Countym: "Maricopa County, AZ"
+// Industry: "Casino/Card Club - Tribal Authorized Casino"
+// State: "Arizona"
+// Year Month: "2014 January"
