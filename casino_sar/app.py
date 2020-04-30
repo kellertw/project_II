6beae1eff28b3e24c1bc845b6e6147fd43051f0a
Kellertw <keller.tw@gmail.com>
2020-04-29 20:37:27 -0400

    update flask structure

--- casino_sar/app.py
+++ casino_sar/app.py
@@ -1,83 +1,29 @@
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from Susp_activity import casino_crime
-import json
-from flask import Flask, jsonify
-
-# For Heroku Deployment
-from flask import (
-    render_template,
-    request,
-    redirect
-    ) 
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-import decimal
-import datetime
-def decoder(obj):
-    if isinstance(obj, datetime.date):
-        return obj.isoformat()
-    elif isinstance(obj, decimal.Decimal):
-        return float(obj)
-      
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///casino.sqlite")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-Keys = Base.classes.keys
-
-CasinoSW = Base.classes.casinoSW
-
-session = Session(engine)
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
-
-# Remove tracking modifications
+# This is the sqlite:///... connection string that you used to connect to the casino db
+app.config['SQLALCHEMY_DATABASE_URI'] = ("sqlite:///Casino.sqlite")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#################################################
-# Flask Routes
-#################################################
-
-@app.route("/")
-def casino():
-    # Create our session (link) from Python to the DB
-
-    # Query all Casino DB
-    results = session.query(CasinoSW.id, CasinoSW.State, CasinoSW.SuspiciousActivity, CasinoSW.Industry, CasinoSW.Long, CasinoSW.Year, CasinoSW.Countym, CasinoSW.Count, CasinoSW.Lat).limit(5).all()
-
-
-    data = []
-    for a, b, c, d, e, f, g, h, i in results:
-        entry = {
-            "id": a,
-            "state": b,
-            "industry": c,
-            "long": decoder(d),
-            "year": decoder(e),
-            "countym": f,
-            "count": g,
-            "lat": decoder(h),
-            "suspicious": decoder(i),
-        }
-        data.append(entry)
-
-    final_results = {"data": data}
-    return final_results
-
+db = SQLAlchemy(app)
+class CasinoSW(db.Model):
+    __tablename__ = 'casinoSW'
+    id = db.Column(db.Integer, primary_key=True)
+    State = db.Column(db.String)
+    SuspiciousActivity = db.Column(db.Float)
+    Industry = db.Column(db.String)
+    Long = db.Column(db.String)
+    Lat = db.Column(db.String)
+    Year = db.Column(db.String)
+    Countym = db.Column(db.String)
+    Count = db.Column(db.String)
+@app.route('/')
+def index():
+    results = CasinoSW.query.limit(5)
+    return render_template('index.html', results=results)
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
---
+++ initdb.py
@@ -0,0 +1,4 @@
+from casino_sar.app import db
+
+# db.drop_all()
+db.create_all()
