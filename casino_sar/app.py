27516cfac8218d50776035817b007f0882613743
Kellertw <keller.tw@gmail.com>
2020-04-30 14:13:23 -0400

    updated app.py, css, about, index

--- casino_sar/app.py
+++ casino_sar/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 # This is the sqlite:///... connection string that you used to connect to the casino db
-app.config['SQLALCHEMY_DATABASE_URI'] = ("sqlite:///Casino.sqlite")
+app.config['SQLALCHEMY_DATABASE_URI'] = ("sqlite:///casino.sqlite")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+
 class CasinoSW(db.Model):
-    __tablename__ = 'casinoSW'
     id = db.Column(db.Integer, primary_key=True)
     State = db.Column(db.String)
     SuspiciousActivity = db.Column(db.Float)
@@ -16,14 +20,13 @@ class CasinoSW(db.Model):
     Year = db.Column(db.String)
     Countym = db.Column(db.String)
     Count = db.Column(db.String)
+
+
 @app.route('/')
 def index():
     results = CasinoSW.query.limit(5)
     return render_template('index.html', results=results)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
