90d0c2e689850543891e60a8d62fa22503ad9ba9
Ghenti Kosumi <gentikosumi@gmail.com>
2020-05-08 13:09:16 -0400

    Changes in app.js, adding pie chart and making some changes in bar chart

--- casino_sar/static/js/app.js
+++ casino_sar/static/js/app.js
@@ -46,12 +46,13 @@ d3.json("/SuspiciousActivity").then(function (importedData) {
   Plotly.newPlot("plotly", chartData, layout);
 });
 
-function buildPlot() {
+// function buildPlot() {
   const url = "/industry";
 
   d3.json(url).then(function (importedData) {
     // console.log(importedData);
     var data = importedData;
+    // console.log(data);
 
     var az = data.filter((d) => d.State === "Arizona");
     var ca = data.filter((d) => d.State === "California");
@@ -59,15 +60,25 @@ function buildPlot() {
     var nv = data.filter((d) => d.State === "Nevada");
     var nm = data.filter((d) => d.State === "New Mexico");
     var ut = data.filter((d) => d.State === "Utah");
+    // console.log(az);
 
     // });
+
+    // var states = [az,ca,co,nv,nm,ut];
+
+    // for (i=0; i<states.length; i++) {
+    //   var labels = states[i].map((s)=>s.Industry);
+    //   var values = states[i].map((s)=>s.Count);
+    //   console.log(labels);
+    //   console.log(values);
 
     // Create an array of labels
     var labels = az.map((a) => a.Industry);
     var values = az.map((a) => a.Count);
-
+  // });
+// }
     // function init() {
-      var chartData = [
+      var data = [
         {
           type: "pie",
           values: values,
@@ -79,45 +90,50 @@ function buildPlot() {
       var layout = {
         showlegend: true,
       };
-      Plotly.newPlot("pie_chart", chartData, layout);
-    });
-  };
-  buildPlot()
+      Plotly.newPlot("pie", data, layout);
+    
+  })
+  //   buildPlot()
+  // };
+    
+  
+  
 
 //   // // On change to the DOM, call getData()
-//   d3.selectAll("#selDataset").on("change", getData);
+  d3.selectAll("#selDataset").on("change", getData);
 
-//   // Function called by DOM changes
-//   function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-//     // Initialize an empty array for the country's data
-//     var data = [];
+  // Function called by DOM changes
+  function getData() {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+    console.log(dataset)
+    // Initialize an empty array for the country's data
+    var data = [];
 
-//     if (dataset == "az") {
-//       data = az;
-//     } else if (dataset == "ca") {
-//       data = ca;
-//     } else if (dataset == "co") {
-//       data = co;
-//     }
-//     if (dataset == "nv") {
-//       data = nv;
-//     } else if (dataset == "nm") {
-//       data = nm;
-//     } else if (dataset == "ut") {
-//       data = ut;
-//     }
+    if (dataset == "az") {
+      data = az;
+    } else if (dataset == "ca") {
+      data = ca;
+    } else if (dataset == "co") {
+      data = co;
+    }
+    if (dataset == "nv") {
+      data = nv;
+    } else if (dataset == "nm") {
+      data = nm;
+    } else if (dataset == "ut") {
+      data = ut;
+    }
 
-//     // Call function to update the chart
-//     updatePlotly(data);
-//   }
+    // Call function to update the chart
+    updatePlotly(data);
+  }
 
-//   // Update the restyled plot's values
-//   function updatePlotly(newdata) {
-//     Plotly.restyle("pie", "values", [newdata]);
-//   }
-// }
+  // Update the restyled plot's values
+  function updatePlotly(newdata) {
+    Plotly.restyle("pie", "values", [newdata]);
+  }
+
 
 // init();
